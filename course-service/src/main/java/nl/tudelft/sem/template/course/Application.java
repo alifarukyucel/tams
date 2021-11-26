d0e135aa55438c41d12eff91865ba1fef1b1b4b5
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-11-26 11:37:36 +0100

    Rename package

--- course-service/src/main/java/nl/tudelft/sem/template/course/Application.java
+++ course-service/src/main/java/nl/tudelft/sem/template/course/Application.java
@@ -1,4 +1,4 @@
-package nl.tudelft.sem.template;
+package nl.tudelft.sem.template.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +8,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("aaaaaaaaaaaaaaaaaaaa");
     }
 
 }
