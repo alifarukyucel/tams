e18e6f6f2b34dd16dd7e9849b38103cea8ddb6c9
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-12-02 18:50:39 +0100

    Use lombok

--- authentication-service/src/main/java/nl/tudelft/sem/template/authentication/models/LoginRequestModel.java
+++ authentication-service/src/main/java/nl/tudelft/sem/template/authentication/models/LoginRequestModel.java
@@ -1,30 +1,9 @@
 package nl.tudelft.sem.template.authentication.models;
 
+import lombok.Data;
+
+@Data
 public class LoginRequestModel {
     private String netid;
     private String password;
-
-    public LoginRequestModel() {
-    }
-
-    public LoginRequestModel(String username, String password) {
-        this.netid = username;
-        this.password = password;
-    }
-
-    public String getNetid() {
-        return netid;
-    }
-
-    public void setNetid(String netid) {
-        this.netid = netid;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- authentication-service/src/main/java/nl/tudelft/sem/template/authentication/models/LoginResponseModel.java
+++ authentication-service/src/main/java/nl/tudelft/sem/template/authentication/models/LoginResponseModel.java
@@ -1,13 +1,8 @@
 package nl.tudelft.sem.template.authentication.models;
 
+import lombok.Data;
+
+@Data
 public class LoginResponseModel {
     private final String token;
-
-    public LoginResponseModel(String token) {
-        this.token = token;
-    }
-
-    public String getToken() {
-        return token;
-    }
 }
