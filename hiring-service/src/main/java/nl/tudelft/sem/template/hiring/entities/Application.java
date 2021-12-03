31c51446db157e5dbcd9b0a7497ce04e7d07a810
Julie <J.D.M.Savelkoul@student.tudelft.nl>
2021-12-03 14:25:55 +0100

    Add columns for application

--- hiring-service/src/main/java/nl/tudelft/sem/template/hiring/entities/Application.java
+++ hiring-service/src/main/java/nl/tudelft/sem/template/hiring/entities/Application.java
@@ -1,4 +1,36 @@
 package nl.tudelft.sem.template.hiring.entities;
 
+import static javax.persistence.GenerationType.SEQUENCE;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "APPLICATION")
 public class Application {
+
+    @Id
+    @Column(name = "COURSE_ID")
+    //Not sure how to deal with PK
+
+    @Column("NETID")
+    //Not sure how to deal with PK
+
+    @Column("MOTIVATION")
+    private String motivation;
+
+    @Column("GRADE")
+    private float grade;
+
+    @Column("STATUS")
+    private String status;
 }
