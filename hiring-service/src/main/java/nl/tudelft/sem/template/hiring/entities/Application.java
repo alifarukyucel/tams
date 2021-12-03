9e35672c3753ae9fe18122d64f77e5fe81483a50
Julie <J.D.M.Savelkoul@student.tudelft.nl>
2021-12-03 14:35:22 +0100

    Add getter and setter to application repository

--- hiring-service/src/main/java/nl/tudelft/sem/template/hiring/entities/Application.java
+++ hiring-service/src/main/java/nl/tudelft/sem/template/hiring/entities/Application.java
@@ -1,10 +1,5 @@
 package nl.tudelft.sem.template.hiring.entities;
 
-import static javax.persistence.GenerationType.SEQUENCE;
-
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.List;
 import javax.persistence.*;
 
 import lombok.AllArgsConstructor;
--- hiring-service/src/main/java/nl/tudelft/sem/template/hiring/repository/ApplicationRepository.java
+++ hiring-service/src/main/java/nl/tudelft/sem/template/hiring/repository/ApplicationRepository.java
@@ -1,4 +1,19 @@
 package nl.tudelft.sem.template.hiring.repository;
 
-public class ApplicationRepository {
+import javax.transaction.Transactional;
+import nl.tudelft.sem.template.hiring.entities.Application;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ApplicationRepository extends JpaRepository<Application, Integer> {
+
+    Application getApplication(int id);
+
+    Application setApplication(Application application);
+
+    void deleteApplication(int id);
+
+
+
 }
