a748dab40a6496192204e402d9339d4b9ea2fb86
Julie <J.D.M.Savelkoul@student.tudelft.nl>
2021-12-03 14:14:37 +0100

    Add repository and controller

---
+++ hiring-service/src/main/java/nl/tudelft/sem/template/hiring/controllers/ApplicationController.java
@@ -0,0 +1,4 @@
+package nl.tudelft.sem.template.hiring.controllers;
+
+public class ApplicationController {
+}
---
+++ hiring-service/src/main/java/nl/tudelft/sem/template/hiring/repository/ApplicationRepository.java
@@ -0,0 +1,4 @@
+package nl.tudelft.sem.template.hiring.repository;
+
+public class ApplicationRepository {
+}
