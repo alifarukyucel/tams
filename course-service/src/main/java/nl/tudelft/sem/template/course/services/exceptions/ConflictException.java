a9556508a6bc5eae3c9a578c63571b3f2be3d80a
Ali Faruk Yucel <a.f.yucel@student.tudelft.nl>
2021-12-06 16:37:18 +0100

    Add ConflictException
    To be thrown when trying to create a course with a pre-existing id

---
+++ course-service/src/main/java/nl/tudelft/sem/template/course/services/exceptions/ConflictException.java
@@ -0,0 +1,25 @@
+package nl.tudelft.sem.template.course.services.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * The Conflict exception.
+ */
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class ConflictException extends RuntimeException {
+    /**
+     * Instantiates a new Conflict exception.
+     */
+    public ConflictException() {
+    }
+
+    /**
+     * Instantiates a new Conflict exception.
+     *
+     * @param message The message.
+     */
+    public ConflictException(String message) {
+        super(message);
+    }
+}
