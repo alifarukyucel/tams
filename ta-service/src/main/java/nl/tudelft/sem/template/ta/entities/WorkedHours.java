177e0e81c40ad073c60aa0b118c452cb82efcf5f
Maurits Kienhuis <m.a.a.kienhuis@student.tudelft.nl>
2021-12-06 18:07:57 +0100

    Add the WorkedHours entity
    And a sneaky fix for the builder pattern in Contract

--- ta-service/src/main/java/nl/tudelft/sem/template/ta/entities/WorkedHours.java
+++ ta-service/src/main/java/nl/tudelft/sem/template/ta/entities/WorkedHours.java
@@ -1,36 +1,34 @@
 package nl.tudelft.sem.template.ta.entities;
 
+import java.util.Date;
 import java.util.UUID;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @Builder
 @Data
 @Entity
-@Table(name = "contracts")
+@Table(name = "workedHours")
 @NoArgsConstructor
 @AllArgsConstructor
-public class Contract {
+public class WorkedHours {
 
     @Id
     @GeneratedValue
     private UUID id;
-
-    int maxHours;
-
-    @Column(nullable = false)
-    String netId;
-
-    @Column(nullable = false)
-    String courseId;
-
-    boolean signed;
+    int workedTime;
+    boolean approved;
+    Date date;
+    String desc;
+    @ManyToOne(optional = false)
+    Contract contract;
 
 }
