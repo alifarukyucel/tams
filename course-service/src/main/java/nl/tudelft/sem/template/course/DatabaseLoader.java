844810e6dbb63a85b92e347b4823824abc6f8f6f
Ali Faruk Yucel <a.f.yucel@student.tudelft.nl>
2021-12-06 17:44:53 +0100

    Add a temporary database loader (just for development)

---
+++ course-service/src/main/java/nl/tudelft/sem/template/course/DatabaseLoader.java
@@ -0,0 +1,28 @@
+package nl.tudelft.sem.template.course;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+import nl.tudelft.sem.template.course.entities.Course;
+import nl.tudelft.sem.template.course.repositories.CourseRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * Loads stuff into the database (only for development).
+ *
+ * @author Ali Faruk Yücel
+ * @version 1.0
+ * @created 06/12/2021 17:25
+ */
+@Service
+public class DatabaseLoader {
+    Date date = new Date();
+
+    public DatabaseLoader(CourseRepository courseRepository) {
+        Course sem = new Course(0, new Date(), "sem", "teaches you swe methods", 99999, new ArrayList<>());
+
+        courseRepository.save(sem);
+    }
+}
