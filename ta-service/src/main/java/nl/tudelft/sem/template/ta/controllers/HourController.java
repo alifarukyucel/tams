21be5d66570a8dd7d3cbcbc27fee83d0853b8af9
Maurits Kienhuis <m.a.a.kienhuis@student.tudelft.nl>
2021-12-07 22:41:26 +0100

    Implement the hour Controller /approve endpoint
    time for testing :sob:

--- ta-service/src/main/java/nl/tudelft/sem/template/ta/controllers/HourController.java
+++ ta-service/src/main/java/nl/tudelft/sem/template/ta/controllers/HourController.java
@@ -1,27 +1,65 @@
 package nl.tudelft.sem.template.ta.controllers;
 
+import java.util.NoSuchElementException;
+import nl.tudelft.sem.template.ta.entities.Contract;
+import nl.tudelft.sem.template.ta.interfaces.CourseInformation;
+import nl.tudelft.sem.template.ta.models.AcceptHoursRequestModel;
 import nl.tudelft.sem.template.ta.security.AuthManager;
 import nl.tudelft.sem.template.ta.services.HourService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("hours")
 public class HourController {
     private final transient AuthManager authManager;
     private final transient HourService hourService;
+    private final transient CourseInformation courseInformation;
 
     /**
      * Instantiates a new HourController.
      *
      * @param authManager the authentication manager
      * @param hourService the hour service
+     * @param courseInformation the course information service
      */
-    public HourController(AuthManager authManager, HourService hourService) {
+    public HourController(AuthManager authManager,
+                          HourService hourService,
+                          CourseInformation courseInformation) {
         this.authManager = authManager;
         this.hourService = hourService;
+        this.courseInformation = courseInformation;
+    }
+
+    /**
+     * Set a worked hour's status to approved.
+     *
+     * @param request The request containing
+     * @return 200 OK if successful
+     */
+    @PutMapping("/approve")
+    public ResponseEntity<String> approve(@RequestBody AcceptHoursRequestModel request) {
+        try {
+            Contract contract = hourService.getAssociatedContract(request.getId());
+
+            if (!courseInformation.isResponsibleLecturer(authManager.getNetid(),
+                contract.getCourseId())) {
+
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+            }
+
+            hourService.approveHours(request.getId(), request.getAccept());
+        } catch (NullPointerException | NoSuchElementException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+
+        return ResponseEntity.ok().build();
     }
 
 
