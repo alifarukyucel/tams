abb4d80f05c3f026449be049421ed18370f9bc8c
Maurits Kienhuis <m.a.a.kienhuis@student.tudelft.nl>
2021-12-08 00:30:57 +0100

    Add profiles

---
+++ ta-service/src/test/java/nl/tudelft/sem/template/ta/profiles/MockTokenVerifierProfile.java
@@ -0,0 +1,27 @@
+package nl.tudelft.sem.template.ta.profiles;
+
+import nl.tudelft.sem.template.ta.security.TokenVerifier;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * A configuration profile to allow injection of a mock TokenGenerator.
+ */
+@Profile("mockTokenVerifier")
+@Configuration
+public class MockTokenVerifierProfile {
+
+    /**
+     * Mocks the TokenGenerator.
+     *
+     * @return A mocked TokenGenerator.
+     */
+    @Bean
+    @Primary
+    public TokenVerifier getMockTokenGenerator() {
+        return Mockito.mock(TokenVerifier.class);
+    }
+}
