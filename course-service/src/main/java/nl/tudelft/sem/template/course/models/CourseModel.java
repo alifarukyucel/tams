43df79a93179e66fcb8af9c65fae0a9cb01b8fa3
Ali Faruk Yucel <a.f.yucel@student.tudelft.nl>
2021-12-07 17:40:11 +0100

    Add CourseModel

---
+++ course-service/src/main/java/nl/tudelft/sem/template/course/models/CourseModel.java
@@ -0,0 +1,35 @@
+package nl.tudelft.sem.template.course.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Ali Faruk Yücel
+ * @version 1.0
+ * @created 07/12/2021, 17:37
+ */
+@Data
+public class CourseModel {
+
+    private String id;
+    private Date startDate;
+    private String name;
+    private String description;
+    private int numberOfStudents;
+    private List<String> responsibleLecturers;
+
+    public CourseModel(String id, Date startDate, String name, String description, int numberOfStudents) {
+        this.id = id;
+        this.startDate = startDate;
+        this.name = name;
+        this.description = description;
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    
+}
