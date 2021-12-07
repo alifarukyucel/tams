943adf2a7e975ffe011de165ed962b8cbedd6f59
Maurits Kienhuis <m.a.a.kienhuis@student.tudelft.nl>
2021-12-07 18:00:06 +0100

    Implement finding the contract based on netId and courseId

--- ta-service/src/main/java/nl/tudelft/sem/template/ta/services/ContractService.java
+++ ta-service/src/main/java/nl/tudelft/sem/template/ta/services/ContractService.java
@@ -1,6 +1,9 @@
 package nl.tudelft.sem.template.ta.services;
 
+import java.util.NoSuchElementException;
 import nl.tudelft.sem.template.ta.entities.Contract;
+import nl.tudelft.sem.template.ta.repositories.ContractRepository;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,6 +12,35 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ContractService {
+
+    private final transient ContractRepository contractRepository;
+
+    public ContractService(ContractRepository contractRepository) {
+        this.contractRepository = contractRepository;
+    }
+
+    /**
+     * Returns the requested contract based on the users netId and the specified CourseId.
+     *
+     * @param netId     The users netId.
+     * @param courseId  The specified course to which the contract belongs.
+     * @return          The contract as specified by the user.
+     * @throws NoSuchElementException Thrown if the contract was not found.
+     */
+    public Contract getContract(String netId, String courseId) throws NoSuchElementException {
+
+        Example<Contract> example = Example.of(
+            Contract.builder()
+            .courseId(courseId)
+            .netId(netId)
+            .build());
+
+        var optionalContract = contractRepository.findOne(example);
+        if (optionalContract.isEmpty()) {
+            throw new NoSuchElementException("The requested contract could not be found");
+        }
+        return optionalContract.get();
+    }
 
 
 }
