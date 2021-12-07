2b22cf0955fd2ed92395e34af2926647f757ca6d
Winstijn Smit <W.Smit-1@student.tudelft.nl>
2021-12-07 16:13:37 +0100

    Added hour and contract service to their controllers

--- ta-service/src/main/java/nl/tudelft/sem/template/ta/controllers/ContractController.java
+++ ta-service/src/main/java/nl/tudelft/sem/template/ta/controllers/ContractController.java
@@ -1,18 +1,27 @@
 package nl.tudelft.sem.template.ta.controllers;
 
+import nl.tudelft.sem.template.ta.entities.Contract;
+import nl.tudelft.sem.template.ta.models.ContractResponseModel;
 import nl.tudelft.sem.template.ta.security.AuthManager;
+import nl.tudelft.sem.template.ta.services.ContractService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("contracts")
 public class ContractController {
     private final transient AuthManager authManager;
+    private final transient ContractService contractService;
 
-    public ContractController(AuthManager authManager) {
+    /**
+     * Instantiates a new ContractController.
+     *
+     * @param authManager the authentication manager
+     * @param contractService the contract service
+     */
+    public ContractController(AuthManager authManager, ContractService contractService) {
         this.authManager = authManager;
+        this.contractService = contractService;
     }
 
 
--- ta-service/src/main/java/nl/tudelft/sem/template/ta/controllers/HourController.java
+++ ta-service/src/main/java/nl/tudelft/sem/template/ta/controllers/HourController.java
@@ -1,6 +1,7 @@
 package nl.tudelft.sem.template.ta.controllers;
 
 import nl.tudelft.sem.template.ta.security.AuthManager;
+import nl.tudelft.sem.template.ta.services.HourService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("hours")
 public class HourController {
     private final transient AuthManager authManager;
+    private final transient HourService hourService;
 
-    public HourController(AuthManager authManager) {
+    /**
+     * Instantiates a new HourController.
+     *
+     * @param authManager the authentication manager
+     * @param hourService the hour service
+     */
+    public HourController(AuthManager authManager, HourService hourService) {
         this.authManager = authManager;
+        this.hourService = hourService;
     }
+
 
     @GetMapping("/test")
     public ResponseEntity<String> createToken() {
