62abfe6e32965ac994211e3f0230e8433ce33306
Julie <J.D.M.Savelkoul@student.tudelft.nl>
2021-12-09 21:53:10 +0100

    Add statusIs()

--- hiring-service/src/main/java/nl/tudelft/sem/template/hiring/entities/enums/ApplicationStatus.java
+++ hiring-service/src/main/java/nl/tudelft/sem/template/hiring/entities/enums/ApplicationStatus.java
@@ -3,5 +3,23 @@ package nl.tudelft.sem.template.hiring.entities.enums;
 public enum ApplicationStatus {
     PENDING,
     ACCEPTED,
-    REJECTED
+    REJECTED;
+
+    public String statusIs() {
+        switch (status)
+        {
+            case PENDING
+                return "pending";
+            case ACCEPTED:
+                return "accepted";
+            case REJECTED:
+                return "rejected";
+        }
+    }
+
+//    public String retrieveStatus(){
+//        if(in process) status = ApplicationStatus.PENDING;
+//        if()
+//    }
+
 }
