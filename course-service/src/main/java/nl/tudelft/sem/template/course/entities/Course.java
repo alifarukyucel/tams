5832c52f26a9f0a2aa6d0b90575d26d1b77bca93
Ali Faruk Yucel <a.f.yucel@student.tudelft.nl>
2021-12-09 15:08:03 +0100

    Change database convention
    According to Microsoft and other best practices:
    https://stackoverflow.com/questions/7662/database-table-and-column-naming-conventions

--- course-service/src/main/java/nl/tudelft/sem/template/course/entities/Course.java
+++ course-service/src/main/java/nl/tudelft/sem/template/course/entities/Course.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.List;
 import javax.persistence.*;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,27 +22,27 @@ import lombok.NoArgsConstructor;
  * @version 1.0
  * @created 30/11/2021, 16:37
  */
-@Data                   // Use Lombok to get rid of getters, setters,
-@NoArgsConstructor      // constructors and other java boilerplate code
-@AllArgsConstructor     // such as toString(), equals(), hashCode()
+@Data                                                // Use Lombok to get rid of getters, setters,
+@NoArgsConstructor(access = AccessLevel.PUBLIC)      // constructors and other java boilerplate code
+@AllArgsConstructor(access = AccessLevel.PUBLIC)     // such as toString(), equals(), hashCode()
 @Entity
-@Table(name = "COURSE")
+@Table(name = "course")
 public class Course {
 
     @Id
-    @Column(name = "ID")
+    @Column(name = "CourseID")
     private String id;
 
-    @Column(name = "START_DATE")
+    @Column(name = "StartDate")
     private Date startDate;
 
-    @Column(name = "NAME")
+    @Column(name = "Name")
     private String name;
 
-    @Column(name = "DESCRIPTION")
+    @Column(name = "Description")
     private String description;
 
-    @Column(name = "NUMBER_OF_STUDENTS")
+    @Column(name = "NumberOfStudents")
     private int numberOfStudents;
 
     @ElementCollection(fetch = FetchType.EAGER)
