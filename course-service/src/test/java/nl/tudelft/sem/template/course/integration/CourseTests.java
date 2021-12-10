81ba5a97550fe6546eacf60edb5a394aa3908e11
Ali Faruk Yucel <a.f.yucel@student.tudelft.nl>
2021-12-10 14:17:30 +0100

    Add MockAuthManager and MockTokenVerifier

--- course-service/src/test/java/nl/tudelft/sem/template/course/integration/CourseTests.java
+++ course-service/src/test/java/nl/tudelft/sem/template/course/integration/CourseTests.java
@@ -3,6 +3,9 @@ package nl.tudelft.sem.template.course.integration;
 import nl.tudelft.sem.template.course.entities.Course;
 import nl.tudelft.sem.template.course.models.CourseModel;
 import nl.tudelft.sem.template.course.repositories.CourseRepository;
+import nl.tudelft.sem.template.course.security.AuthManager;
+import nl.tudelft.sem.template.course.security.TokenVerifier;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -18,6 +22,8 @@ import java.time.LocalDateTime;
 
 import static nl.tudelft.sem.template.course.utils.JsonUtil.serialize;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles({"test", "mockTokenVerifier", "mockAuthenticationManager"})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
 public class CourseTests {
@@ -35,12 +42,28 @@ public class CourseTests {
     @Autowired
     private transient CourseRepository courseRepository;
 
+    @Autowired
+    private transient TokenVerifier mockTokenVerifier;
+
+    @Autowired
+    private transient AuthManager mockAuthenticationManager;
+
+
     final static String testCourseID = "CSE2115";
     final static LocalDateTime testStartDate = LocalDateTime.of(2021, 12, 1, 0, 0);
     final static String testCourseName = "SEM";
     final static String testDescription = "swe methods";
     final static int testNumberOfStudents = 300;
     final static String responsibleLecturer = "fmulder";
+
+    @BeforeEach
+    void setUp() {
+        courseRepository.deleteAll();
+        // Save a basic course in db.
+        when(mockAuthenticationManager.getNetid()).thenReturn(responsibleLecturer);
+        when(mockTokenVerifier.validate(anyString())).thenReturn(true);
+        when(mockTokenVerifier.parseNetid(anyString())).thenReturn(responsibleLecturer);
+    }
 
     @Test
     public void createCourse_withValidData_worksCorrectly() throws Exception {
