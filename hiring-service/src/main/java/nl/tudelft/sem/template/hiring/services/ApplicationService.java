911dc770edae0a82e57af4f0dff34c2b2808d4d7
Julie <J.D.M.Savelkoul@student.tudelft.nl>
2021-12-10 16:58:56 +0100

    Add retrieveStatus in service

--- hiring-service/src/main/java/nl/tudelft/sem/template/hiring/services/ApplicationService.java
+++ hiring-service/src/main/java/nl/tudelft/sem/template/hiring/services/ApplicationService.java
@@ -2,6 +2,7 @@ package nl.tudelft.sem.template.hiring.services;
 
 import nl.tudelft.sem.template.hiring.Example;
 import nl.tudelft.sem.template.hiring.entities.Application;
+import nl.tudelft.sem.template.hiring.entities.compositeKeys.ApplicationKey;
 import nl.tudelft.sem.template.hiring.entities.enums.ApplicationStatus;
 import nl.tudelft.sem.template.hiring.repositories.ApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 /**
  * ApplicationService
@@ -22,9 +24,9 @@ public class ApplicationService {
     @Autowired
     private transient ApplicationRepository applicationRepository;
 
-    public ApplicationRepository(ApplicationRepository applicationRepository) {
-        this.applicationRepository = applicationRepository;
-    }
+//    public ApplicationRepository(ApplicationRepository applicationRepository) {
+//        this.applicationRepository = applicationRepository;
+//    }
 
 
     /**
@@ -42,35 +44,63 @@ public class ApplicationService {
         }
     }
 
-    public Application getApplication(String netId, String courseId) throws NoSuchElementException {
-        if(courseId == null || netId == null){
-             throw new NoSuchElementException("An application must have a netId and courseId");
-        }
-    }
+//    public Application getApplication(String netId, String courseId) throws NoSuchElementException {
+//        if(courseId == null || netId == null){
+//             throw new NoSuchElementException("An application must have a netId and courseId");
+//        }
+//    }
 
+
+//    /**
+//     * Retrieving the status of the application to see whether someone is accepted or rejected.
+//     * @param application the application to retrieve the status from
+//     * @param applicationStatus the actual status of the application
+//     * @return String containing the status in readable format
+//     */
+//    public String retrieveStatus(Application application, ApplicationStatus applicationStatus) throws NoSuchElementException {
+//        if(applicationStatus.equals(ApplicationStatus.PENDING)) {
+//            application.setStatus(ApplicationStatus.PENDING);
+//            applicationRepository.save(application);
+//            applicationRepository.getOne()
+//            return "The selection procedure is still pending";
+//        }
+//        if(applicationStatus.equals(ApplicationStatus.REJECTED)){
+//            application.setStatus(ApplicationStatus.REJECTED);
+//            applicationRepository.save(application);
+//            return "Unfortunately you have been rejected";
+//        }
+//        if(applicationStatus.equals(ApplicationStatus.ACCEPTED)) {
+//            application.setStatus(ApplicationStatus.ACCEPTED);
+//            applicationRepository.save(application);
+//            return "Congratulations, you have been accepted";
+//        }
+//        else {
+//            throw new NoSuchElementException("There is no application ");
+//        }
+//    }
 
     /**
      * Retrieving the status of the application to see whether someone is accepted or rejected.
-     * @param application the application to retrieve the status from
-     * @param applicationStatus the actual status of the application
-     * @return String containing the status in readable format
+     * @param key the key to find the application in the database.
+     * @return String containing the status in readable format.
+     * @throws NoSuchElementException
      */
-    public String retrieveStatus(Application application, ApplicationStatus applicationStatus) throws NoSuchElementException {
-        if(applicationStatus.equals(ApplicationStatus.PENDING)) {
-            application.setStatus(ApplicationStatus.PENDING);
+
+
+    public String retrieveStatus(ApplicationKey key) throws NoSuchElementException {
+        Optional<Application> application = applicationRepository.findById(key);
+        if(!application.isPresent()) throw new NoSuchElementException("Application does not exist");
+        Application actualApplication = application.get();
+        ApplicationStatus status = actualApplication.getStatus();
+        if(status == null) {
+            actualApplication.setStatus(ApplicationStatus.PENDING);
             return "The selection procedure is still pending";
         }
-        if(applicationStatus.equals(ApplicationStatus.REJECTED)){
-            application.setStatus(ApplicationStatus.REJECTED);
-            return "Unfortunately you have been rejected";
-        }
-        if(applicationStatus.equals(ApplicationStatus.ACCEPTED)) {
-            application.setStatus(ApplicationStatus.ACCEPTED);
+        if(status.equals(ApplicationStatus.ACCEPTED)) {
             return "Congratulations, you have been accepted";
         }
-        else {
-            throw new NoSuchElementException("There is no application ");
+        else{
+            return "Unfortunately you have been rejected";
         }
     }
-    
 }
