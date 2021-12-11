1c3d10cb31ba594954eb6b9d5cb1afb0aae0bcb2
Ali Faruk Yucel <a.f.yucel@student.tudelft.nl>
2021-12-11 11:21:55 +0100

    Fix forgotten import

--- course-service/src/main/java/nl/tudelft/sem/template/course/controllers/CourseController.java
+++ course-service/src/main/java/nl/tudelft/sem/template/course/controllers/CourseController.java
@@ -9,6 +9,7 @@ import nl.tudelft.sem.template.course.security.AuthManager;
 import nl.tudelft.sem.template.course.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
