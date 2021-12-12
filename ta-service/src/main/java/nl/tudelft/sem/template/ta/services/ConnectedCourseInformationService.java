27d2b2f81e7db0f799d6a319bbe82fa08c23c384
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-12-13 00:39:21 +0200

    Import base url in CourseInformationService

--- ta-service/src/main/java/nl/tudelft/sem/template/ta/services/ConnectedCourseInformationService.java
+++ ta-service/src/main/java/nl/tudelft/sem/template/ta/services/ConnectedCourseInformationService.java
@@ -1,11 +1,15 @@
 package nl.tudelft.sem.template.ta.services;
 
 import nl.tudelft.sem.template.ta.interfaces.CourseInformation;
-import org.springframework.beans.factory.annotation.Qualifier;
+import nl.tudelft.sem.template.ta.services.models.CourseInformationResponseModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConnectedCourseInformationService implements CourseInformation {
+    @Value("${microservice.course.base_url}")
+    private transient String baseUrl;
 
     @Override
     public boolean isResponsibleLecturer(String netId, String courseId) {
