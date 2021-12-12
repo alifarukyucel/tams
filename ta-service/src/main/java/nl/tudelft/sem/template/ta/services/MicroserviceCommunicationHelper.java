5b07c26ba83461b6bf03b25f1adc97bb134c3d48
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-12-13 00:45:04 +0200

    Add javadoc in MicroserviceCommunicationHelper

--- ta-service/src/main/java/nl/tudelft/sem/template/ta/services/MicroserviceCommunicationHelper.java
+++ ta-service/src/main/java/nl/tudelft/sem/template/ta/services/MicroserviceCommunicationHelper.java
@@ -18,6 +18,20 @@ public class MicroserviceCommunicationHelper {
         this.request = request;
     }
 
+    /**
+     * Make a request to a URL with the request and response type headers as well as the authentication
+     * header pre-configured.
+     *
+     * @param url          The URL to make a request to
+     * @param responseType The (expected) type of the response
+     * @param body         The body of the request
+     * @param method       The HTTP method
+     * @param variables    The variables to be inserted into the URL
+     * @param <T>          The type of the request
+     * @param <J>          The type of the response
+     * @return The response
+     * @throws Exception if the status code is not 200 OK
+     */
     public <T, J> ResponseEntity<T> send(String url, Class<T> responseType, J body,
                                          HttpMethod method, String... variables) {
         RestTemplate restTemplate = new RestTemplate();
@@ -36,14 +50,51 @@ public class MicroserviceCommunicationHelper {
         return response;
     }
 
+    /**
+     * Make a GET request to a URL with the request and response type headers as well as the authentication
+     * header pre-configured.
+     *
+     * @param url          The URL to make a request to
+     * @param responseType The (expected) type of the response
+     * @param variables    The variables to be inserted into the URL
+     * @param <T>          The type of the request
+     * @return The response
+     * @throws Exception if the status code is not 200 OK
+     */
     public <T> ResponseEntity<T> get(String url, Class<T> responseType, String... variables) {
         return send(url, responseType, null, HttpMethod.GET, variables);
     }
 
+    /**
+     * Make a POST to a URL with the request and response type headers as well as the authentication
+     * header pre-configured.
+     *
+     * @param url          The URL to make a request to
+     * @param responseType The (expected) type of the response
+     * @param body         The body of the request
+     * @param variables    The variables to be inserted into the URL
+     * @param <T>          The type of the request
+     * @param <J>          The type of the response
+     * @return The response
+     * @throws Exception if the status code is not 200 OK
+     */
     public <T, J> ResponseEntity<T> post(String url, Class<T> responseType, J body, String... variables) {
         return send(url, responseType, body, HttpMethod.POST, variables);
     }
 
+    /**
+     * Make a PUT to a URL with the request and response type headers as well as the authentication
+     * header pre-configured.
+     *
+     * @param url          The URL to make a request to
+     * @param responseType The (expected) type of the response
+     * @param body         The body of the request
+     * @param variables    The variables to be inserted into the URL
+     * @param <T>          The type of the request
+     * @param <J>          The type of the response
+     * @return The response
+     * @throws Exception if the status code is not 200 OK
+     */
     public <T, J> ResponseEntity<T> put(String url, Class<T> responseType, J body, String... variables) {
         return send(url, responseType, body, HttpMethod.PUT, variables);
     }
