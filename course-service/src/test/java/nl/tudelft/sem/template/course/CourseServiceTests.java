23616248a71e350f75789e0d5ac5007ce0ae7ca2
Ali Faruk Yucel <a.f.yucel@student.tudelft.nl>
2021-12-13 21:10:15 +0100

    Fix typo

--- course-service/src/test/java/nl/tudelft/sem/template/course/CourseServiceTests.java
+++ course-service/src/test/java/nl/tudelft/sem/template/course/CourseServiceTests.java
@@ -55,7 +55,7 @@ public class CourseServiceTests {
                 testDescription, testNumberOfStudents, responsibleLecturers);
 
         // Act
-        courseService.save(course);
+        courseRepository.save(course);
 
         // Assert
         Course expected = courseService.getCourseById(testCourseId);
