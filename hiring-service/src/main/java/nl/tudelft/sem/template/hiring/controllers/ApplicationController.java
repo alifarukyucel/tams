40281eaa22797fb167bed438178a4a7cb349eaf8
Julie <J.D.M.Savelkoul@student.tudelft.nl>
2021-12-13 21:03:26 +0100

    Add put mapping

--- hiring-service/src/main/java/nl/tudelft/sem/template/hiring/controllers/ApplicationController.java
+++ hiring-service/src/main/java/nl/tudelft/sem/template/hiring/controllers/ApplicationController.java
@@ -9,6 +9,7 @@ import nl.tudelft.sem.template.hiring.services.ApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,5 +46,10 @@ public class ApplicationController {
         } else {
             return ResponseEntity.badRequest().build();
         }
+    }
+
+    @PutMapping("/withdraw")
+    public ResponseEntity<String> withdraw(@RequestBody ApplicationRequestModel request) {
+
     }
 }
