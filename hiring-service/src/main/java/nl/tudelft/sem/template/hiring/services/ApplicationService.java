be70d61662f42de621a4a1de955ace94b7864db9
Julie <J.D.M.Savelkoul@student.tudelft.nl>
2021-12-13 21:02:48 +0100

    Add withdraw method to service

--- hiring-service/src/main/java/nl/tudelft/sem/template/hiring/services/ApplicationService.java
+++ hiring-service/src/main/java/nl/tudelft/sem/template/hiring/services/ApplicationService.java
@@ -1,15 +1,24 @@
 package nl.tudelft.sem.template.hiring.services;
 
 import nl.tudelft.sem.template.hiring.entities.Application;
+import nl.tudelft.sem.template.hiring.interfaces.CourseInformation;
 import nl.tudelft.sem.template.hiring.repositories.ApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 
 @Service
 public class ApplicationService {
 
     @Autowired
     private transient ApplicationRepository applicationRepository;
+    private final transient CourseInformation courseInformation;
+
+    public ApplicationService(CourseInformation courseInformation) {
+        this.courseInformation = courseInformation;
+    }
 
 
     /**
@@ -25,5 +34,17 @@ public class ApplicationService {
         } else {
             return false;
         }
+    }
+
+    /**
+     * Deletes an application from the database, if more than 3 weeks before start of the course
+     * @param application the application to withdraw
+     */
+    public void withdraw(Application application) {
+        LocalDate deadline = courseInformation.startDate(application.getCourseId()).minusWeeks(3);
+        if(LocalDate.now().compareTo(deadline) < 0) {
+            applicationRepository.delete(application);
+        }
+        return;
     }
 }
--- hiring-service/src/main/java/nl/tudelft/sem/template/hiring/services/ConnectedCourseInformationService.java
+++ hiring-service/src/main/java/nl/tudelft/sem/template/hiring/services/ConnectedCourseInformationService.java
@@ -1,10 +1,7 @@
 package nl.tudelft.sem.template.hiring.services;
 
 import nl.tudelft.sem.template.hiring.interfaces.CourseInformation;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
-
-import java.sql.Time;
 import java.time.LocalDate;
 
 @Service
