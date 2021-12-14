8d5c3a32e4ad64a82bcc3b8aa065c60bf6d70b8c
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-12-14 18:47:21 +0200

    Add builder to ApplicationAcceptRequestModel

--- hiring-service/src/main/java/nl/tudelft/sem/template/hiring/models/ApplicationAcceptRequestModel.java
+++ hiring-service/src/main/java/nl/tudelft/sem/template/hiring/models/ApplicationAcceptRequestModel.java
@@ -1,9 +1,11 @@
 package nl.tudelft.sem.template.hiring.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
