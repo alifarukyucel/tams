f8a171c6e2304a77eaa03ca2ca0e7a107ac92c48
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-12-14 21:56:29 +0200

    Add profile definition to ContractServiceTest

--- ta-service/src/test/java/nl/tudelft/sem/template/ta/services/ContractServiceTest.java
+++ ta-service/src/test/java/nl/tudelft/sem/template/ta/services/ContractServiceTest.java
@@ -16,12 +16,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @Transactional  // prevents lazy loading issues and will keep the connection open.
+@ActiveProfiles({"test"})
 class ContractServiceTest {
 
     @Autowired
