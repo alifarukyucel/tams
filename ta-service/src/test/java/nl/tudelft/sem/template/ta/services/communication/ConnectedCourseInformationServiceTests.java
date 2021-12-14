d64dea49c5ca1e25e9649ed97549f9d68b981928
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-12-14 14:30:02 +0200

    Fix import order

--- ta-service/src/test/java/nl/tudelft/sem/template/ta/services/communication/ConnectedCourseInformationServiceTests.java
+++ ta-service/src/test/java/nl/tudelft/sem/template/ta/services/communication/ConnectedCourseInformationServiceTests.java
@@ -1,5 +1,12 @@
 package nl.tudelft.sem.template.ta.services.communication;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import nl.tudelft.sem.template.ta.services.communication.models.CourseInformationResponseModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,13 +15,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ActiveProfiles({"test", "mockMicroserviceCommunicationHelper"})
