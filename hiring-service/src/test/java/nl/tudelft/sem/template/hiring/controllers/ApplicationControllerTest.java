6418a82f32017c2d7b8a8bc71421d322776cab4b
Julie <J.D.M.Savelkoul@student.tudelft.nl>
2021-12-14 17:39:11 +0100

    Add tests for controller

--- hiring-service/src/test/java/nl/tudelft/sem/template/hiring/controllers/ApplicationControllerTest.java
+++ hiring-service/src/test/java/nl/tudelft/sem/template/hiring/controllers/ApplicationControllerTest.java
@@ -21,6 +21,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.security.Key;
+import java.util.Optional;
+
 import static nl.tudelft.sem.template.hiring.utils.JsonUtil.serialize;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -121,72 +124,49 @@ public class ApplicationControllerTest {
     }
 
     @Test
-    public void getStatusByCourseTest() throws Exception {
-        //Arrange
-
-        // Your function /get status doesn't take a model so you don't need to use one.
-        RetrieveStatusModel pendingModel = new RetrieveStatusModel(
-                "CSE1200", "kverhoef", "I like TAs",
-                9, ApplicationStatus.PENDING);
-        RetrieveStatusModel acceptedModel = new RetrieveStatusModel(
-                "CSE1200", "dsmith", "I like TAs",
-                9, ApplicationStatus.ACCEPTED);
-        RetrieveStatusModel rejectedModel = new RetrieveStatusModel(
-                "CSE1200", "lbrown", "I like TAs",
-                9, ApplicationStatus.REJECTED);
-
-        ApplicationKey pendingKey = new ApplicationKey(pendingModel.getCourseId(), exampleNetId);
-        ApplicationKey acceptedKey = new ApplicationKey(acceptedModel.getCourseId(), exampleNetId);
-        ApplicationKey rejectedKey = new ApplicationKey(rejectedModel.getCourseId(), exampleNetId);
-
-        //Act
-        // This isn't used by anything.
-        ResultActions validResults = mockMvc.perform(post("/apply")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(serialize(pendingModel))
-                .header("Authorization", "Bearer Joe"));
-
-        // below actions throw user defined error, does not exist.
-        // reason for this is that the netId is fetched from the authmanager which returns johndoe, This person is not saved in the db.
-
-        // what you want to be doing is the following
-        when(mockAuthenticationManager.getNetid()).thenReturn("kverhoef");
-        // you do this because the auth manager is what is used to get the net id.
-        // and the default is not used in this test, hence we want to change it.
-        // Note the netId is the same one as used in the setUp() function above as this is a pending apllication.
-
-        ResultActions pendingResult  = mockMvc.perform(get("/status/CSE1200").header("Authorization", "Bearer Joe"));
-        ResultActions acceptedResult = mockMvc.perform(get("/status/CSE1200").header("Authorization", "Bearer Joe"));
-        ResultActions rejectedResult = mockMvc.perform(get("/status/CSE1200").header("Authorization", "Bearer Joe"));
-
-        //Assert
-        pendingResult.andExpect(status().isOk());
-        acceptedResult.andExpect(status().isOk());
-        rejectedResult.andExpect(status().isOk());
-
-        // these asserts are checking your hardcoded variables from above.
-        // ApplicationKey pendingKey = new ApplicationKey(pendingModel.getCourseId(), exampleNetId);
-        // I assume you are trying to chech the repsonse from the server, in which case you want to deserialize the result you obtain.
-        // There are of examples in other integration tests that go into it.
-        // But you will need to use Martin's json util
-        assertThat(applicationRepository.findById(pendingKey).get().getStatus().equals(ApplicationStatus.PENDING));
-        assertThat(applicationRepository.findById(acceptedKey).get().getStatus().equals(ApplicationStatus.ACCEPTED));
-        assertThat(applicationRepository.findById(rejectedKey).get().getStatus().equals(ApplicationStatus.REJECTED));
-    }
-
-    @Test
-    void exampleTest() throws Exception {
-        // What you want to be doing is writing smaller tests and focus on those.
-
+    void pendingStatusTest() throws Exception {
         // arrange
         when(mockAuthenticationManager.getNetid()).thenReturn("kverhoef");
+        ApplicationKey key = new ApplicationKey(pendingApplication.getCourseId(), pendingApplication.getNetId());
 
         // act
         ResultActions pendingResult  = mockMvc.perform(get("/status/CSE1200").header("Authorization", "Bearer Joe"));
 
         // assert
         pendingResult.andExpect(status().isOk());
-        // other asserts removed for brevity
-        // But in reality you would want to also check the returned types.
+        assertThat(pendingApplication.getStatus()).isEqualTo(ApplicationStatus.PENDING);
+        assertThat(applicationRepository.findById(key).get().getStatus()).isEqualTo(ApplicationStatus.PENDING);
     }
+
+    @Test
+    void acceptedStatusTest() throws Exception {
+        // arrange
+        when(mockAuthenticationManager.getNetid()).thenReturn("kverhoef");
+        ApplicationKey key = new ApplicationKey(acceptedApplication.getCourseId(), acceptedApplication.getNetId());
+
+        // act
+        ResultActions pendingResult  = mockMvc.perform(get("/status/CSE1200").header("Authorization", "Bearer Joe"));
+
+        // assert
+        pendingResult.andExpect(status().isOk());
+        assertThat(acceptedApplication.getStatus()).isEqualTo(ApplicationStatus.ACCEPTED);
+        assertThat(applicationRepository.findById(key).get().getStatus()).isEqualTo(ApplicationStatus.ACCEPTED);
+    }
+
+    @Test
+    void rejectedStatusTest() throws Exception {
+        // arrange
+        when(mockAuthenticationManager.getNetid()).thenReturn("kverhoef");
+        ApplicationKey key = new ApplicationKey(rejectedApplication.getCourseId(), rejectedApplication.getNetId());
+
+        // act
+        ResultActions pendingResult  = mockMvc.perform(get("/status/CSE1200").header("Authorization", "Bearer Joe"));
+
+        // assert
+        pendingResult.andExpect(status().isOk());
+        assertThat(rejectedApplication.getStatus()).isEqualTo(ApplicationStatus.REJECTED);
+        assertThat(applicationRepository.findById(key).get().getStatus()).isEqualTo(ApplicationStatus.REJECTED);
+    }
+
+
 }
