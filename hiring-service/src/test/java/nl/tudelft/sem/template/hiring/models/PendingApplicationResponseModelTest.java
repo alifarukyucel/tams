2758018e27705db800dda24a29b849ef5e430695
Mattheo de Wit <M.C.A.deWit@student.tudelft.nl>
2021-12-14 22:06:00 +0100

    add model test.

---
+++ hiring-service/src/test/java/nl/tudelft/sem/template/hiring/models/PendingApplicationResponseModelTest.java
@@ -0,0 +1,27 @@
+package nl.tudelft.sem.template.hiring.models;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import nl.tudelft.sem.template.hiring.entities.Application;
+import nl.tudelft.sem.template.hiring.entities.enums.ApplicationStatus;
+import org.junit.jupiter.api.Test;
+
+public class PendingApplicationResponseModelTest {
+    @Test
+    public void PendingApplicationResponseModelConstructorTest() {
+        //Arrange
+        Application application = new Application("CSE1300", "jsmith", 7.0f,
+                "I want to be cool too!", ApplicationStatus.PENDING);
+
+        //Act
+        var model = new PendingApplicationResponseModel(application, 8.0f);
+
+        var expected = new PendingApplicationResponseModel("CSE1300", "jsmith", 7.0f,
+                "I want to be cool too!", 8.0f);
+
+        //Assert
+        assertThat(model).isEqualTo(expected);
+    }
+
+
+}
