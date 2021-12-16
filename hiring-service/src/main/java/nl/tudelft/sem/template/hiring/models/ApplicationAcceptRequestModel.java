bc61140637d78f25286bcd63714eb7a7b1980daa
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-12-16 11:50:01 +0200

    Add builder for ApplicationAcceptRequestModel

--- hiring-service/src/main/java/nl/tudelft/sem/template/hiring/models/ApplicationAcceptRequestModel.java
+++ hiring-service/src/main/java/nl/tudelft/sem/template/hiring/models/ApplicationAcceptRequestModel.java
@@ -1,11 +1,9 @@
 package nl.tudelft.sem.template.hiring.models;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,4 +12,45 @@ public class ApplicationAcceptRequestModel {
     private String netId;
     private String duties;
     private int maxHours;
+
+    public static ApplicationAcceptRequestModelBuilder builder() {
+        return new ApplicationAcceptRequestModelBuilder();
+    }
+
+    /*
+     * Builder for the ApplicationAcceptRequestModel.
+     */
+    public static class ApplicationAcceptRequestModelBuilder {
+        private String courseId;
+        private String netId;
+        private String duties;
+        private int maxHours;
+
+        ApplicationAcceptRequestModelBuilder() {
+        }
+
+        public ApplicationAcceptRequestModelBuilder courseId(String courseId) {
+            this.courseId = courseId;
+            return this;
+        }
+
+        public ApplicationAcceptRequestModelBuilder netId(String netId) {
+            this.netId = netId;
+            return this;
+        }
+
+        public ApplicationAcceptRequestModelBuilder duties(String duties) {
+            this.duties = duties;
+            return this;
+        }
+
+        public ApplicationAcceptRequestModelBuilder maxHours(int maxHours) {
+            this.maxHours = maxHours;
+            return this;
+        }
+
+        public ApplicationAcceptRequestModel build() {
+            return new ApplicationAcceptRequestModel(courseId, netId, duties, maxHours);
+        }
+    }
 }
