ad2f6740e04787be5f14e38cc9396bc429523aec
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-12-16 11:47:31 +0200

    Add builder for CreateContractRequestModel

--- hiring-service/src/main/java/nl/tudelft/sem/template/hiring/services/communication/models/CreateContractRequestModel.java
+++ hiring-service/src/main/java/nl/tudelft/sem/template/hiring/services/communication/models/CreateContractRequestModel.java
@@ -2,11 +2,9 @@ package nl.tudelft.sem.template.hiring.services.communication.models;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Builder
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @AllArgsConstructor
 @Data
@@ -15,4 +13,45 @@ public class CreateContractRequestModel {
     private String netId;
     private String duties; 
     private int maxHours;
+
+    public static CreateContractRequestModelBuilder builder() {
+        return new CreateContractRequestModelBuilder();
+    }
+
+    /*
+     * Builder for the CreateContractRequestModel.
+     */
+    public static class CreateContractRequestModelBuilder {
+        private String courseId;
+        private String netId;
+        private String duties;
+        private int maxHours;
+
+        CreateContractRequestModelBuilder() {
+        }
+
+        public CreateContractRequestModelBuilder courseId(String courseId) {
+            this.courseId = courseId;
+            return this;
+        }
+
+        public CreateContractRequestModelBuilder netId(String netId) {
+            this.netId = netId;
+            return this;
+        }
+
+        public CreateContractRequestModelBuilder duties(String duties) {
+            this.duties = duties;
+            return this;
+        }
+
+        public CreateContractRequestModelBuilder maxHours(int maxHours) {
+            this.maxHours = maxHours;
+            return this;
+        }
+
+        public CreateContractRequestModel build() {
+            return new CreateContractRequestModel(courseId, netId, duties, maxHours);
+        }
+    }
 }
