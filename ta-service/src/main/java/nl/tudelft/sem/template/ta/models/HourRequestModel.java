e0db06c1c35ffd9ca953784f8c8f986f1eebd8d1
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-12-16 11:48:51 +0200

    Add builder for HourRequestModel

--- ta-service/src/main/java/nl/tudelft/sem/template/ta/models/HourRequestModel.java
+++ ta-service/src/main/java/nl/tudelft/sem/template/ta/models/HourRequestModel.java
@@ -2,15 +2,42 @@ package nl.tudelft.sem.template.ta.models;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @AllArgsConstructor
 public class HourRequestModel {
     private String course;
     private String netId;
+
+    public static HourRequestModelBuilder builder() {
+        return new HourRequestModelBuilder();
+    }
+
+    /*
+     * Builder for the HourRequestModel.
+     */
+    public static class HourRequestModelBuilder {
+        private String course;
+        private String netId;
+
+        HourRequestModelBuilder() {
+        }
+
+        public HourRequestModelBuilder course(String course) {
+            this.course = course;
+            return this;
+        }
+
+        public HourRequestModelBuilder netId(String netId) {
+            this.netId = netId;
+            return this;
+        }
+
+        public HourRequestModel build() {
+            return new HourRequestModel(course, netId);
+        }
+    }
 }
