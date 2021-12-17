1fd9227dca64013a0ef525cdf57164658744e5d2
Julie <J.D.M.Savelkoul@student.tudelft.nl>
2021-12-17 15:11:51 +0100

    Add first test

--- ta-service/src/test/java/nl/tudelft/sem/template/ta/entities/builders/HourDeclarationBuilderTest.java
+++ ta-service/src/test/java/nl/tudelft/sem/template/ta/entities/builders/HourDeclarationBuilderTest.java
@@ -1,4 +1,27 @@
 package nl.tudelft.sem.template.ta.entities.builders;
 
+import nl.tudelft.sem.template.ta.entities.HourDeclaration;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class HourDeclarationBuilderTest {
+
+    private HourDeclaration hourDeclaration;
+
+    @Test
+    public void testWithId() {
+        //arrange
+        UUID id = UUID.randomUUID();
+
+        //act
+        hourDeclaration = new ConcreteHourDeclarationBuilder()
+                .withId(id)
+                .build();
+
+        //assert
+        assertThat(hourDeclaration.getId()).isEqualTo(id);
+    }
 }
