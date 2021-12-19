f19ed5d128c4fed16a2ea6a578505fe22eafe7aa
Julie <J.D.M.Savelkoul@student.tudelft.nl>
2021-12-19 15:52:10 +0100

    Add tests for directors

--- ta-service/src/test/java/nl/tudelft/sem/template/ta/entities/builders/HourDeclarationBuilderTest.java
+++ ta-service/src/test/java/nl/tudelft/sem/template/ta/entities/builders/HourDeclarationBuilderTest.java
@@ -6,11 +6,47 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 import nl.tudelft.sem.template.ta.entities.Contract;
 import nl.tudelft.sem.template.ta.entities.HourDeclaration;
+import nl.tudelft.sem.template.ta.entities.builders.directors.ContractDirector;
+import nl.tudelft.sem.template.ta.entities.builders.directors.HourDeclarationDirector;
 import org.junit.jupiter.api.Test;
 
 public class HourDeclarationBuilderTest {
 
     private HourDeclaration hourDeclaration;
+
+    @Test
+    public void testDirector() {
+        //arrange
+        UUID id = UUID.randomUUID();
+        Integer workedTime = 60;
+        LocalDateTime date = LocalDateTime.now();
+        String desc = "description";
+        Contract contract = new ConcreteContractBuilder()
+                .withNetId("kverhoef")
+                .withCourseId("CSE1300")
+                .withMaxHours(8)
+                .withDuties("DUTIES")
+                .withSigned(true)
+                .withRating(2.0)
+                .build();
+
+        var builder = new ConcreteHourDeclarationBuilder();
+        new HourDeclarationDirector().createUnsignedContract(builder);
+
+
+        //act
+        hourDeclaration = builder
+                .withId(id)
+                .withWorkedTime(workedTime)
+                .withDate(date)
+                .withDescription(desc)
+                .withContractId(contract)
+                .build();
+
+        //arrange
+        assertThat(hourDeclaration.getApproved()).isFalse();
+        assertThat(hourDeclaration.getReviewed()).isFalse();
+    }
 
     @Test
     public void testWithId() {
