be598266e2dd3485f41ff65ba24903b93ae2cd90
Julie <J.D.M.Savelkoul@student.tudelft.nl>
2021-12-22 14:56:29 +0100

    Add comment to FORBIDDEN

--- hiring-service/src/main/java/nl/tudelft/sem/template/hiring/controllers/ApplicationController.java
+++ hiring-service/src/main/java/nl/tudelft/sem/template/hiring/controllers/ApplicationController.java
@@ -59,6 +59,7 @@ public class ApplicationController {
     @PostMapping("/apply")
     public ResponseEntity<String> apply(@RequestBody ApplicationRequestModel request) {
         if (applicationService.hasReachedMaxApplication(authManager.getNetid())) {
+            // It is not allowed to have more than 3 applications
             throw new ResponseStatusException(HttpStatus.FORBIDDEN);
         }
         Application application = createPendingApplication(
