f5ed9f6f43c897a857332b231c06fb9a76d91697
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-01-04 22:48:34 +0200

    Make comments clearer

--- authentication-service/src/main/java/nl/tudelft/sem/template/authentication/providers/TimeProvider.java
+++ authentication-service/src/main/java/nl/tudelft/sem/template/authentication/providers/TimeProvider.java
@@ -4,6 +4,8 @@ import java.time.Instant;
 
 /**
  * An abstract time provider to make services testable.
+ * This interface can be mocked in order to provide a predetermined current time and
+ * make tests independent of the actual current time.
  */
 public interface TimeProvider {
     /**
--- authentication-service/src/main/java/nl/tudelft/sem/template/authentication/providers/implementations/CurrentTimeProvider.java
+++ authentication-service/src/main/java/nl/tudelft/sem/template/authentication/providers/implementations/CurrentTimeProvider.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 
 /**
  * An abstract time provider to make services testable.
+ * The TimeProvider interface can be mocked in order to provide a predetermined current time and
+ * make tests independent of the actual current time.
  */
 @Component
 public class CurrentTimeProvider implements TimeProvider {
