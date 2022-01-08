0cbfae13de3a5a7c62fd67edc88ac1a2f2756eca
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-01-08 15:19:27 +0100

    Rename package of hiring microservice

---
+++ hiring-microservice/src/main/java/nl/tudelft/sem/tams/hiring/models/RetrieveTeachingAssistantApplicationStatusModel.java
@@ -0,0 +1,35 @@
+package nl.tudelft.sem.tams.hiring.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nl.tudelft.sem.tams.hiring.entities.TeachingAssistantApplication;
+import nl.tudelft.sem.tams.hiring.entities.enums.ApplicationStatus;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RetrieveTeachingAssistantApplicationStatusModel {
+    private String courseId;
+    private String netid;
+    private String motivation;
+    private float grade;
+    private ApplicationStatus status;
+
+    /**
+     * Constructor that constructs a RetrieveStatusModel from an application and courseId.
+     *
+     * @param teachingAssistantApplication the application to retrieve a status from
+     * @return RetrieveStatusModel of given application
+     */
+    public static RetrieveTeachingAssistantApplicationStatusModel fromApplication(
+            TeachingAssistantApplication teachingAssistantApplication) {
+        return new RetrieveTeachingAssistantApplicationStatusModel(
+                teachingAssistantApplication.getCourseId(),
+                teachingAssistantApplication.getNetId(),
+                teachingAssistantApplication.getMotivation(),
+                teachingAssistantApplication.getGrade(),
+                teachingAssistantApplication.getStatus()
+        );
+    }
+}
