e008b6f610bcbc17b5dfd1f39876aa776d4b2039
Ali Faruk Yucel <a.f.yucel@student.tudelft.nl>
2022-01-08 22:58:47 +0300

    Add CourseController

--- course-microservice/src/main/java/nl/tudelft/sem/tams/course/controllers/CourseController.java
+++ course-microservice/src/main/java/nl/tudelft/sem/tams/course/controllers/CourseController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -31,7 +32,7 @@ public class CourseController {
     private final transient AuthManager authManager;
 
     private final transient CourseService courseService;
-    
+
     /**
      * Instantiates a new Course controller.
      *
@@ -51,7 +52,7 @@ public class CourseController {
      * @param id            id of course
      * @return the course found in the database with the given id
      */
-    @GetMapping("/{id}")
+    @GetMapping("/{id}") // id
     public ResponseEntity<CourseResponseModel> getCourseById(@PathVariable String id)
             throws NoSuchElementException {
         try {
@@ -71,9 +72,9 @@ public class CourseController {
      * @return 200 OK with true if user is responsible lecturer of the course
      *         200 OK with false if not.
      */
-    @GetMapping("{courseId}/lecturer/{netId}")
+    @GetMapping("{courseId}/lecturer/{netId}") // {courseId}/lecturer/{netId}
     public ResponseEntity<Boolean> isResponsibleLecturer(@PathVariable String netId,
-                                                        @PathVariable String courseId) {
+                                                         @PathVariable String courseId) {
         try {
             courseService.isResponsibleLecturer(netId, courseId);
             return ResponseEntity.ok(true);
@@ -92,8 +93,8 @@ public class CourseController {
      * @param courseModel   the course to be created
      * @return the course returned from the database (with a manually-assigned id)
      */
-    @PostMapping(value = "/create", consumes = "application/json")
-    ResponseEntity<CourseResponseModel> createCourse(@RequestBody CourseCreationRequestModel courseModel)
+    @PostMapping(value = "/create", consumes = "application/json") // create
+    public ResponseEntity<CourseResponseModel> createCourse(@RequestBody CourseCreationRequestModel courseModel)
             throws ResponseStatusException {
         Course course = new Course(courseModel.getId(),
                 courseModel.getStartDate(), courseModel.getName(),
@@ -102,6 +103,53 @@ public class CourseController {
         courseService.createCourse(course);
         CourseResponseModel response = CourseResponseModel.fromCourse(course);
         return ResponseEntity.ok(response);
+    }
+
+    /**
+     * PUT endpoint that adds the given netId as a responsible lecturer to the given course.
+     *
+     * @param courseId          Id of the course to add responsible lecturers to
+     * @param netId             NetId of the responsible lecturer to be added
+     * @return                  403 FORBIDDEN if the requesting user is not a responsible lecturer
+     *                          404 NOT FOUND if the course does not exist
+     *                          200 OK if given netId is successfully added
+     */
+    @PutMapping(value = "{courseId}/addLecturer/{netId}")   // {courseId}/addLecturer/{netId}
+    public ResponseEntity<String> addResponsibleLecturers(@PathVariable String courseId,
+                                                          @PathVariable String netId) {
+        try {
+            courseService.isResponsibleLecturer(authManager.getNetid(), courseId);
+        } catch (NoSuchElementException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+                    "You should be a responsible lecturer to execute this operation.");
+        }
+        courseService.addResponsibleLecturers(courseId, netId); // this can't throw NoSuchElementException since
+        // whether a course exists or not is already checked by isResponsibleLecturer.
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * PUT endpoint that adds the given netIds as responsible lecturers to the given course.
+     * Effectively replaces the existing course's responsible lecturers field with the new one.
+     *
+     * @param courseId          Id of the course to add responsible lecturers to
+     * @param model             Course object with updated responsible lecturers
+     * @return                  403 FORBIDDEN if the requesting user is not a responsible lecturer
+     *                          404 NOT FOUND if the course does not exist
+     *                          200 OK if given netId is successfully added
+     */
+    @PutMapping(value = "{courseId}/addLecturer/")   // {courseId}/addLecturer/
+    public ResponseEntity<String> addResponsibleLecturers(@PathVariable String courseId,
+                                                          @RequestBody CourseAddResponsibleLecturerRequestModel model) {
+        try {
+            courseService.isResponsibleLecturer(authManager.getNetid(), courseId);
+        } catch (NoSuchElementException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+                    "You should be a responsible lecturer to execute this operation.");
+        }
+        courseService.addResponsibleLecturers(courseId, model.getResponsibleLecturers()); // this can't throw
+        // NoSuchElementException since whether a course exists or not is already checked by isResponsibleLecturer.
+        return ResponseEntity.ok().build();
     }
 
     // ---------------------------------- Deletions -------------------------------
