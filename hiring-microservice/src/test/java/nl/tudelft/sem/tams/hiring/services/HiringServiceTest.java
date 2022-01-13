3c7add09dd74c896022bf2280860cff4756761e7
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-01-13 19:11:46 +0100

    Revert "Update HiringServiceTest to use getCourseById instead of startDate"
    This reverts commit 80713a36

--- hiring-microservice/src/test/java/nl/tudelft/sem/tams/hiring/services/HiringServiceTest.java
+++ hiring-microservice/src/test/java/nl/tudelft/sem/tams/hiring/services/HiringServiceTest.java
@@ -555,15 +555,7 @@ public class HiringServiceTest {
         taApplicationRepository.save(teachingAssistantApplication);
 
         //LocalDateTime.MAX is used here to guarantee the deadline hasn't passed yet
-        when(mockCourseInformation.getCourseById(teachingAssistantApplication.getCourseId())).thenReturn(
-                new CourseInformationResponseModel(
-                        "CSE1200",
-                        LocalDateTime.MAX,
-                        "CourseName",
-                        "CourseDescription",
-                        100,
-                        new ArrayList<>())
-        );
+        when(mockCourseInformation.startDate(teachingAssistantApplication.getCourseId())).thenReturn(LocalDateTime.MAX);
 
         //Act
         boolean result = taApplicationService.checkAndWithdraw(
@@ -583,15 +575,7 @@ public class HiringServiceTest {
         taApplicationRepository.save(teachingAssistantApplication);
 
         //When the startTime is the current time, the deadline has passed already
-        when(mockCourseInformation.getCourseById(teachingAssistantApplication.getCourseId()))
-                .thenReturn(new CourseInformationResponseModel(
-                        "CSE1200",
-                        assumedCurrentTime,
-                        "CourseName",
-                        "CourseDescription",
-                        100,
-                        new ArrayList<>())
-            );
+        when(mockCourseInformation.startDate(teachingAssistantApplication.getCourseId())).thenReturn(assumedCurrentTime);
 
         //Act
         boolean result = taApplicationService.checkAndWithdraw(
@@ -612,15 +596,8 @@ public class HiringServiceTest {
                 "CSE1300", "jsmith", 7.0f,
                 motivation, ApplicationStatus.PENDING);
         taApplicationRepository.save(teachingAssistantApplication);
-        when(mockCourseInformation.getCourseById(teachingAssistantApplication.getCourseId())).thenReturn(
-                new CourseInformationResponseModel(
-                        "CSE1200",
-                        assumedCurrentTime.plusWeeks(3),
-                        "CourseName",
-                        "CourseDescription",
-                        100,
-                        new ArrayList<>())
-        );
+        when(mockCourseInformation.startDate(teachingAssistantApplication.getCourseId()))
+                .thenReturn(assumedCurrentTime.plusWeeks(3));
 
         //Act
         boolean result = taApplicationService.checkAndWithdraw(
@@ -641,15 +618,8 @@ public class HiringServiceTest {
                 "CSE1300", "jsmith", 7.0f,
                 motivation, ApplicationStatus.PENDING);
         taApplicationRepository.save(teachingAssistantApplication);
-        when(mockCourseInformation.getCourseById(teachingAssistantApplication.getCourseId())).thenReturn(
-                new CourseInformationResponseModel(
-                        "CSE1200",
-                        assumedCurrentTime.plusWeeks(3).plusNanos(1),
-                        "CourseName",
-                        "CourseDescription",
-                        100,
-                        new ArrayList<>())
-        );
+        when(mockCourseInformation.startDate(teachingAssistantApplication.getCourseId())).thenReturn(
+                assumedCurrentTime.plusWeeks(3).plusNanos(1));
 
         //Act
         boolean result = taApplicationService.checkAndWithdraw(
